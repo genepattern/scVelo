15677074d39e2e7ba53571df6950e03ee3e8461e
Anthony S. Castanza <acastanza@ucsd.edu>
2021-09-20 14:47:05 -0700

    Revamp to output plots

--- src/compute_scvelo.py
+++ src/compute_scvelo.py
@@ -39,11 +39,13 @@ def main():
                     dest="input_file", help="h5ad (anndata) file.")
     ap.add_argument("-m", "--markers", action="store",
                     dest="markers", nargs='?', help="A list of marker genes")
+    ap.add_argument("-v", "--velmode", default="stochastic", action="store", dest="velocity_mode",
+                    help="Mode for performing the velocity estimation. 'deterministic', 'stochastic' (default), or 'dynamical'")
     ap.add_argument("-s", "--shared", default="30", action="store", dest="minshared",
                     help="Filter genes by minimum shared counts")
     ap.add_argument("-t", "--top", default="2000", action="store", dest="topgenes",
                     help="Top genes for velocity Computation")
-    ap.add_argument("-v", "--hvg", default="True", action="store", dest="hvg",
+    ap.add_argument("-g", "--hvg", default="True", action="store", dest="hvg",
                     help="Recalculate highly_variable_genes")
     ap.add_argument("-e", "--embedding", default="umap", action="store", dest="embedding",
                     help="Dataset was processed with umap or tsne embedding")
@@ -52,11 +54,13 @@ def main():
     ap.add_argument("-n", "--neighbors", default="30", action="store", dest="neighbors",
                     help="Number of nearest neighbors in PCA space used for computing gene moments")
     ap.add_argument("-d", "--diffkin", default="True", action="store", dest="diff_kinetics",
-                    help="Perform differential kinetics analysis using clustering.")
+                    help="Perform differential kinetics analysis using clustering (requires 'dynamical' mode velocity estimation).")
     ap.add_argument("-f", "--force", default="False", action="store", dest="enforce",
                     help="Enforce normalizaion using scvelo's internal functions.")
     ap.add_argument("-p", "--plot", default="png", action="store", dest="plot",
                     help="Save velocity plots as png or svg")
+    ap.add_argument("-b", "--batch", default="True", action="store", dest="plot_batches",
+                    help="Produce individual velocity plots for each batch in the dataset (if present)")
     ap.add_argument("-o", "--out", default="result", action="store",
                     dest="output", help="Output file basename")
 #	ap.add_argument("-j","--cpu",action="store",dest="ncores",help="CPU cores to use for transition dynamics calculation")
@@ -90,9 +94,13 @@ def main():
             options.minshared), n_top_genes=int(options.topgenes), layers_normalize={'spliced', 'unspliced'})
     scv.pp.moments(adata, n_pcs=int(options.pcs),
                    n_neighbors=int(options.neighbors))
-    scv.tl.recover_dynamics(adata)  # , n_jobs=int(options.ncores))
-    scv.tl.velocity(adata, mode='dynamical')
+
+    if options.velocity_mode == "dynamical":
+        scv.tl.recover_dynamics(adata)  # , n_jobs=int(options.ncores))
+
+    scv.tl.velocity(adata, mode=options.velocity_mode)
     scv.tl.velocity_graph(adata)
+    scv.tl.velocity_pseudotime(adata)
 
     # Confirm presence of lower dimensional embeddings and generate if absent
     if options.embedding != "tsne":
@@ -106,7 +114,8 @@ def main():
                 "'tSNE' Embedding was requested, but we didn't find it in the dataset so creating it now.\n")
             scv.tl.tsne(adata)
 #	scv.tl.louvain(adata)
-    scv.tl.latent_time(adata)
+    if options.velocity_mode == "dynamical":
+        scv.tl.latent_time(adata)
 
 # Detect/create clustering
     if "clusters" in list(adata.obs):
@@ -135,20 +144,27 @@ def main():
               cluster_out + ".txt", sep="\t")
 
 # Plotting
+    if options.velocity_mode == "dynamical":
+        plots = ['velocity_pseudotime', 'latent_time', cluster_type]
+    else:
+        plots = ['velocity_pseudotime', cluster_type]
 
-    scv.pl.velocity_embedding_stream(adata, color=['latent_time', cluster_type], color_map='gnuplot',
-                                     basis=options.embedding, save=options.output + "_latent_time_velocity_embedding." + options.plot)
     if "batch" in list(adata.obs):
         batches = list(adata.obs['batch'].cat.categories)
         scv.pl.velocity_embedding_stream(
-            adata, color='batch', basis=options.embedding, save=options.output + "_batches_velocity_embedding." + options.plot)
-        for i in batches:
-            try:
-                scv.pl.velocity_embedding_stream(adata[adata.obs['batch'] == i], color=['latent_time', cluster_type], color_map='gnuplot',
-                                                 basis=options.embedding, save=options.output + "_" + i + "_latent_time_velocity_embedding." + options.plot)
-            except ValueError:
-                warnings.warn(print("Unable to plot batch: " + i +
-                                    ". Perhaps too many cells were removed by filtering parameters."))
+            adata, color=plots+['batch'], basis=options.embedding, save=options.output + "_velocity_embedding." + options.plot)
+        if options.plot_batches == "True":
+            for i in batches:
+                try:
+                    scv.pl.velocity_embedding_stream(adata[adata.obs['batch'] == i], color=plots, color_map='gnuplot',
+                                                     basis=options.embedding, save=options.output + "_" + i + "_velocity_embedding." + options.plot)
+                except ValueError:
+                    warnings.warn(print("Unable to plot batch: " + i +
+                                        ". Perhaps too many cells were removed by filtering parameters."))
+    else:
+        scv.pl.velocity_embedding_stream(adata, color=plots, color_map='gnuplot',
+                                         basis=options.embedding, save=options.output + "_velocity_embedding." + options.plot)
+
 
     scv.tl.velocity_confidence(adata)
     scv.pl.scatter(adata, c=['velocity_length'], cmap='coolwarm', perc=[
@@ -174,26 +190,35 @@ def main():
             adata[:, velocity_genes_list].var['fit_diff_kinetics'])
         scv.pl.scatter(adata, legend_loc='right', size=60, title='diff kinetics',
                        add_outline=diff_clusters, outline_width=(.8, .2), color=cluster_type, save=options.output + "_outlined_differential_kinetics_clusters." + options.plot)
+        scv.tl.recover_dynamics(adata)
         scv.tl.velocity(adata, mode='dynamical', diff_kinetics=True)
         scv.tl.velocity_graph(adata)
-        scv.pl.velocity_embedding_stream(adata, color=['latent_time', cluster_type], color_map='gnuplot',
-                                         basis=options.embedding, save=options.output + "_differential_kinetics" + "_latent_time_velocity_embedding." + options.plot)
+        scv.tl.velocity_pseudotime(adata)
+        scv.tl.latent_time(adata)
+        plots = ['velocity_pseudotime', 'latent_time', cluster_type]
+
         if "batch" in list(adata.obs):
             batches = list(adata.obs['batch'].cat.categories)
             scv.pl.velocity_embedding_stream(
-                adata, color='batch', basis=options.embedding, save=options.output + "_differential_kinetics" + "_batches_velocity_embedding." + options.plot)
-            for i in batches:
-                try:
-                    scv.pl.velocity_embedding_stream(adata[adata.obs['batch'] == i], color=['latent_time', cluster_type], color_map='gnuplot',
-                                                     basis=options.embedding, save=options.output + "_differential_kinetics" + "_" + i + "_latent_time_velocity_embedding." + options.plot)
-                except ValueError:
-                    warnings.warn(print("Unable to plot batch: " + i +
-                                        ". Perhaps too many cells were removed by filtering parameters."))
+                adata, color=plots+['batch'], basis=options.embedding, save=options.output + "_differential_kinetics" + "_velocity_embedding." + options.plot)
+            if options.plot_batches == "True":
+                for i in batches:
+                    try:
+                        scv.pl.velocity_embedding_stream(adata[adata.obs['batch'] == i], color=plots, color_map='gnuplot',
+                                                         basis=options.embedding, save=options.output + "_differential_kinetics" + "_" + i + "_velocity_embedding." + options.plot)
+                    except ValueError:
+                        warnings.warn(print("Unable to plot batch: " + i +
+                                            ". Perhaps too many cells were removed by filtering parameters."))
+        else:
+            scv.pl.velocity_embedding_stream(adata, color=plots, color_map='gnuplot',
+                                             basis=options.embedding, save=options.output + "_differential_kinetics" + "_velocity_embedding." + options.plot)
+
         scv.tl.velocity_confidence(adata)
         scv.pl.scatter(adata, c=['velocity_length'], cmap='coolwarm', perc=[
                        5, 95], save=options.output + "_differential_kinetics" + "_velocity_length_embedding." + options.plot)
         scv.pl.scatter(adata, c=['velocity_confidence'], cmap='coolwarm', perc=[
                        5, 95], save=options.output + "_differential_kinetics" + "_velocity_confidence_embedding." + options.plot)
+        scv.tl.velocity_pseudotime(adata)
 
     ad.AnnData.write(adata, compression="gzip",
                      filename=options.output + "_complete_velocity_data.h5ad")
