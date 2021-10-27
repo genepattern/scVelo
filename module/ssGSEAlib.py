b977fceea9c32ca7af3fe9f9603b16b2f96c9911
Anthony S. Castanza <acastanza@ucsd.edu>
2021-10-26 17:43:29 -0700

    Progress on gene set scoring

--- module/ssGSEAlib.py
+++ module/ssGSEAlib.py
@@ -185,7 +185,80 @@ def project_to_geneset(
     # exponential weight applied to ranking in calculation of
     # enrichment score
     weight=0):
-    print("Not Working Yet")
+
+    gene_names = data_array.index.tolist()
+    n_rows = data_array.shape[0]
+    n_cols = data_array.shape[1]
+
+
+    ES_vector = [False]*n_cols
+    ranked_expression = [0]*n_rows
+
+    # Compute ES score for signatures in each sample
+    for sample_index in range(n_cols):
+        # gene.list is permutation (list of row indices) of the normalized expression data, where
+        # permutation places expression data in decreasing order
+        # Note that in ssGSEA we rank genes by their expression level rather than by a measure of correlation
+        # between expression profile and phenotype.
+        gene_list = (-data_array.iloc[:,sample_index].to_numpy()).argsort()
+
+        # gene.set2 contains the indices of the matching genes.
+        # Note that when input GCT file is ATARiS-generated, elements of
+        # gene.names may not be unique; the following code insures each element
+        # of gene.names that is present in the gene.set is referenced in gene.set2
+        gene_set2 = np.array(range(len(gene_names)))[list(np.isin(gene_names, gene_set))]
+
+        # transform the normalized expression data for a single sample into ranked (in decreasing order)
+        # expression values
+        sys.exit("Not Working Yet")
+        if (weight == 0) {
+            # don't bother doing calcuation, just set to 1
+            ranked.expression <- rep(1, n.rows)
+        } else if (weight > 0) {
+            # calculate z.score of normalized (e.g., ranked) expression values
+            x <- data.array[gene.list, sample.index]
+            ranked.expression <- (x - mean(x))/sd(x)
+        }
+
+        # tag.indicator flags, within the ranked list of genes, those that are in the gene set
+        tag.indicator <- sign(match(gene.list, gene.set2, nomatch=0))    # notice that the sign is 0 (no tag) or 1 (tag)
+        no.tag.indicator <- 1 - tag.indicator
+        N <- length(gene.list)
+        Nh <- length(gene.set2)
+        Nm <-  N - Nh
+        # ind are indices into ranked.expression, whose values are in decreasing order, corresonding to
+        # genes that are in the gene set
+        ind = which(tag.indicator==1)
+        ranked.expression <- abs(ranked.expression[ind])^weight
+
+        sum.ranked.expression = sum(ranked.expression)
+        # "up" represents the peaks in the mountain plot; i.e., increments in the running-sum
+        up = ranked.expression/sum.ranked.expression
+        # "gaps" contains the lengths of the gaps between ranked pathway genes
+        gaps = (c(ind-1, N) - c(0, ind))
+        # "down" contain the valleys in the mountain plot; i.e., the decrements in the running-sum
+        down = gaps/Nm
+        # calculate the cumulative sums at each of the ranked pathway genes
+        RES = cumsum(c(up,up[Nh])-down)
+        valleys = RES[1:Nh]-up
+
+        max.ES = max(RES)
+        min.ES = min(valleys)
+
+        if( max.ES > -min.ES ){
+            arg.ES <- which.max(RES)
+        } else {
+            arg.ES <- which.min(RES)
+        }
+        # calculates the area under RES by adding up areas of individual
+        # rectangles + triangles
+        gaps = gaps+1
+        RES = c(valleys,0) * (gaps) + 0.5*( c(0,RES[1:Nh]) - c(valleys,0) ) * (gaps)
+        ES = sum(RES)
+        ES.vector[sample.index] <- ES
+
+    return list(ES_vector))
+# end of Project.to.GeneSet
 
 
 
